87051ebb597d42f3e5d227705ae78fff50a9a1e6
choijaegwon <chl9338@naver.com>
2023-10-09 23:27:50 +0900

    FB 로그아웃기능 구현

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/home';
@@ -11,9 +13,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(SignoutRequestedEvent());
+              },
+              icon: Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/bloc_logo_full.png',
+                width: MediaQuery.of(context).size.width * 0.8,
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Bloc is an awesome\nstate management library\nfor flutter!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
